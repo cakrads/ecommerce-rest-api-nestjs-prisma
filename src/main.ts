43b04489fc9d308d911c0d871ed5d8e96b247bbb
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-26 09:46:48 +0700

    feat: add Prisma module and service

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
