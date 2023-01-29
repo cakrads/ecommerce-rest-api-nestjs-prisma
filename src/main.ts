4331d8f2c39cbf4842881b547ce1464e96521fce
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-30 05:56:27 +0700

    feat: install nest/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app.module';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
+  const config = new DocumentBuilder()
+    .setTitle('Simple Ecommerce')
+    .setDescription('The Simple Ecommerce REST API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3000);
 }
