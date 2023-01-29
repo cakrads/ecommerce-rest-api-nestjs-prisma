2833c65d84ef5d3d429c9310b828a663c60645c3
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-30 06:47:39 +0700

    feat: add PrismaClient to the Products module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
+  imports: [PrismaModule],
 })
 export class ProductsModule {}
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createProductDto: CreateProductDto) {
     return `This action adds a new product ${JSON.stringify(createProductDto)}`;
   }
