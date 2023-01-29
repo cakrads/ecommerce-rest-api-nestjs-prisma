6292975c9015e4a20bf4c49f419770d624e63c0e
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-30 06:04:53 +0700

    fix: wrong import products module path

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [PrismaModule, ProductsModule],
