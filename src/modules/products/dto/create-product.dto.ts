b3dd88f8b6bf2817ca6dd2a037965aafa8265ce6
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-01-30 23:51:11 +0700

    feat: update create and update product DTO

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,36 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateProductDto {
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  subtitle?: string;
+
+  @ApiProperty()
+  slug: string;
+
+  @ApiProperty()
+  context: string;
+
+  @ApiProperty()
+  description?: string;
+
+  @ApiProperty()
+  image?: string[];
+
+  // @ApiProperty()
+  // price?: Price;
+
+  // @ApiProperty()
+  // categories?: Category;
+
+  // @ApiProperty()
+  // brands?: Brand
+
+  @ApiProperty({ required: false, default: '' })
+  source?: string = '';
+
+  @ApiProperty({ required: false, default: true })
+  published?: boolean = true;
+}
