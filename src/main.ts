0a7aef06af7f4020ec77e98e0d749edbd1bfa5f2
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-02-03 23:14:16 +0700

    feat: add global prefix '/api'

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Simple Ecommerce')
