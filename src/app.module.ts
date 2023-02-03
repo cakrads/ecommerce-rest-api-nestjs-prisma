307fa514c8e6f6f679993ca33b6d1fc81322f18a
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-02-04 06:42:20 +0700

    feat: add ServeStaticModule to access image from URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -6,8 +9,14 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './modules/products';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    ProductsModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
