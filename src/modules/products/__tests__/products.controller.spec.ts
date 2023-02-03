6979014b81451ac711675973a5633186ea5eb493
Cakra Danu Sedayu <cakra.ds@gmail.com>
2023-02-03 23:18:31 +0700

    fix: path to Products Controller and Service

--- src/modules/products/__tests__/products.controller.spec.ts
+++ src/modules/products/__tests__/products.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ProductsController } from './products.controller';
-import { ProductsService } from './products.service';
+import { ProductsController } from './../products.controller';
+import { ProductsService } from './../products.service';
 
 describe('ProductsController', () => {
   let controller: ProductsController;
